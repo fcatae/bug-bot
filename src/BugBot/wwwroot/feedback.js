80a1dfd8668a54dc884def12d9f3262c16559243
Fabricio Catae <fcatae@microsoft.com>
2016-08-19 01:14:27 -0300

    add feedback report page

---
+++ src/BugBot/wwwroot/feedback.js
@@ -0,0 +1,11 @@
+﻿
+open iframe using #DataCue
+
+
+myIframe.contentWindow.postMessage('hello', '*');
+and in the iframe:
+    window.onmessage = function(e){
+        if (e.data == 'hello') {
+            alert('It works!');
+        }
+    };
---
+++ src/BugBot/wwwroot/template.js
@@ -0,0 +1,23 @@
+﻿function init(user,feedback,form,url) {
+    //var user = document.getElementById('user');
+    //var feedback = document.getElementById('feedback');
+
+    user.value = getUserId();
+    form.action = url;
+
+    alert(user.value)
+}
+
+
+function submitForm(feedback, user) {
+    setUserId();
+
+    return true;
+}
+
+function getUserId() {
+    var hash = document.location.hash;    
+    var username = (hash) ? hash.substring(1) : '';
+
+    return encodeURIComponent(username);
+}
