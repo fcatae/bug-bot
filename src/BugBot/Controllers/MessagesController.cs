b65157ff8af9487a1b8836669d3f17cded3523d1
Fabricio Catae <fcatae@microsoft.com>
2016-09-01 18:47:33 -0300

    clean up

--- src/BugBot/Controllers/MessagesController.cs
+++ src/BugBot/Controllers/MessagesController.cs
@@ -26,21 +26,7 @@ namespace BugBot.Controllers
             this._dataActivity = dataActivity;
             this._eventActivity = eventActivity;
         }
-
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
+        
         // POST api/values
         [HttpPost]
         public void Post([FromBody] Activity activity)
